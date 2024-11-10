f3cfd6e483b63ffce27554cf5d3b180ec6babc42
ffzayac <fedja-zajac@bk.ru>
2024-11-10 12:11:56 +0300

    Add wardrobe_items_app

--- wardrobe_app/wardrobe_app/settings.py
+++ wardrobe_app/wardrobe_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wardrobe_items_app.apps.WardrodeItemsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ wardrobe_app/wardrode_items_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WardrodeItemsAppConfig(AppConfig):
+    name = 'wardrode_items_app'
